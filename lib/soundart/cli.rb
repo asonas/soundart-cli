9412af55aaacbd4fa3e139672ee8ab02cc53d121
Yuya Fujiwara <asonas@cookpad.com>
2019-08-11 19:51:06 +0900

    Initial commit

---
+++ lib/soundart/cli.rb
@@ -0,0 +1,8 @@
+require "soundart/cli/version"
+
+module Soundart
+  module Cli
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/soundart/cli/version.rb
@@ -0,0 +1,5 @@
+module Soundart
+  module Cli
+    VERSION = "0.1.0"
+  end
+end
